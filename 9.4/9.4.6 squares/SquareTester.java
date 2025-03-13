065d967519aca745122dbfbe9277cb3d8d843064
pooperheadcoolman <jason14bneare@gmail.com>
2025-03-13 11:38:31 -0700

    Create SquareTester.java

---
+++ 9.4/9.4.6 squares/SquareTester.java
@@ -0,0 +1,9 @@
+public class SquareTester
+{
+    public static void main(String[] args)
+    {
+        Square box = new Square(5);
+        System.out.println(box);
+        System.out.println(box.area());
+    }
+}
